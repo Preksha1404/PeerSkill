73cdc8c4b45589db382c90f425f7abe43847ba7a
Preksha <ranapreksha8@gmail.com>
2025-08-01 13:05:53 +0530

    feat: set up Express server with MongoDB connection and error handling

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import app from "./app.js"
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 3000}`);
+        });
+        app.on("error", (err) => {
+            console.error("Express server error:", err);
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    })
